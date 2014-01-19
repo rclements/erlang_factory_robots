917c573e953d2a9feff4cb643fc55e4949eae63c
rclements <rclements@isotope11.com>
2014-01-19 23:32:14 +0000

    Add tcp server for led

---
+++ rgb_led/examples/tcp_led.erl
@@ -0,0 +1,40 @@
+-module(tcp_led).
+-export([start/3]).
+
+start(Red, Green, Blue) ->
+  {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0}, 
+                                      {active, false}]),
+  do_accept(LSock, {Red, Green, Blue}).
+
+do_accept(LSock, {Red, Green, Blue}) ->
+  {ok, Sock} = gen_tcp:accept(LSock),
+  {ok, Pid} = rgb_led_server:start(Red, Green, Blue),
+  do_recv(Sock, Pid),
+  do_accept(LSock, {Red, Green, Blue}).
+
+do_recv(Sock, Pid) ->
+  case gen_tcp:recv(Sock, 0) of
+    {ok, Data} ->
+      handle_data(Data, Pid),
+      do_recv(Sock, Pid);
+    {error, closed} ->
+      ok
+  end.
+
+handle_data(Data, Pid) ->
+  {Red, Green, Blue} = extract_values(Data),
+  Pid ! {update, [{red, Red}, {green, Green}, {blue, Blue}]},
+  Pid ! blast.
+
+extract_values(Data) ->
+  {match, [Red, Green, Blue]} = re:run(Data, value_extractor(), [{capture, ['RED', 'GREEN', 'BLUE'], list}]),
+  {input_to_num(Red), input_to_num(Green), input_to_num(Blue)}.
+
+value_extractor() ->
+  "R(?<RED>[0-9.]+)G(?<GREEN>[0-9.]+)B(?<BLUE>[0-9.]).*".
+
+input_to_num(N) ->
+  case string:to_float(N) of
+    {error,no_float} -> list_to_integer(N);
+    {F,_Rest} -> F
+  end.
