00075ef72471743482ca8d7c1bf4526d3009110d
Josh Adams <josh@isotope11.com>
2014-01-22 00:39:35 -0600

    send :os.cmd a list

--- elixir/rgb_led/lib/pi_blaster.ex
+++ elixir/rgb_led/lib/pi_blaster.ex
@@ -1,7 +1,7 @@
 defmodule PiBlaster do
   def set_pin(pin, value) do
     IO.inspect "#{build_command(pin, value)}"
-    build_command(pin, value) |> :os.cmd
+    build_command(pin, value) |> bitstring_to_list |>:os.cmd
   end
 
   def build_command(pin, value) do
